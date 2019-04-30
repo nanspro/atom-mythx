b9bd34df90e29756702b104d6bfbb6d65b5fdf6e
fnatic <rahulbishnoi3636@gmail.com>
2019-04-30 11:45:58 +0530

    Sample bytecode

---
+++ spec/atom-mythx-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMythxView from '../lib/atom-mythx-view';
+
+describe('AtomMythxView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
